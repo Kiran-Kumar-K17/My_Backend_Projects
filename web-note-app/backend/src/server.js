7c6a55ccf36173eb6b4eafe7ca40d3a71086575e
Kiran Kumar <kirankumark1707@gmail.com>
2025-12-18 19:38:17 +0530

    feat: add Navbar

--- web-note-app/backend/src/server.js
+++ web-note-app/backend/src/server.js
@@ -2,18 +2,21 @@ import express from "express";
 import notesRoute from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 
 dotenv.config();
-connectDB();
 
 //Middleware
 app.use(express.json());
+app.use(rateLimiter);
 
 const PORT = process.env.PORT;
 app.use("/api/notes", notesRoute);
 
-app.listen(PORT, () => {
-  console.log(`server started at port: ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server started at port: ${PORT}`);
+  });
 });
